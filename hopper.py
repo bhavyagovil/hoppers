9b2405711b78db2d00ab3826f03a075a03f5daec
hyperbolic-potato147 <hyperbolicpotato@protonmail.com>
2024-10-19 10:35:55 +0100

    Add files via upload

--- hopper.py
+++ hopper.py
@@ -1,1 +1,35 @@
 import pygame
+
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 600
+WINDOW_TITLE = "Hopper"
+
+GRID_TILE_SIZE = 40
+GRID_NUM_COLS = 15
+GRID_NUM_ROWS = 15
+
+pygame.init()
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption(WINDOW_TITLE)
+
+class Bunny(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pass
+
+class Fox(pygame.sprite.Sprite):
+    def __init__(self):
+        pass
+
+class Adalovelace(pygame.sprite.Sprite):
+    def __init__(self):
+        pass
+
+class GameMap():
+    def __init__(self):
+        pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
