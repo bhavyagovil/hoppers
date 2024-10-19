f11251ee526402ff8faa1d557b45d0cfb021a2d5
RheaBose <146022502+RheaBose@users.noreply.github.com>
2024-10-19 14:44:57 +0100

    Add files via upload

---
+++ trees.py
@@ -0,0 +1,18 @@
+class Tree(pygame.sprite.Sprite):
+    def __init__(self, x, y, image_path, *groups):
+        super().__init__(*groups)
+        self.image = pygame.image.load(image_path).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (2 * GRID_TILE_SIZE, 2 * GRID_TILE_SIZE))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y) 
+        
+trees = [
+    Tree(0, 0, 'tree.png', all_sprites),
+    Tree(0, 0, 'tree.png', all_sprites),
+    Tree(0, 0, 'tree.png', all_sprites),
+    Tree(0, 0, 'tree.png', all_sprites),
+    Tree(0, 0, 'tree.png', all_sprites)
+]
+
+for tree in trees:
+    all_sprites.add(tree)
